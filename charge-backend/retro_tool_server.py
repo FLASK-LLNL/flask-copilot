73985fc9c97370c6bfa96fca4ed29dc708e41187
Shehtab <szaman5@binghamton.edu>
2025-10-12 20:08:35 -0400

    Update path

--- charge-backend/retro_tool_server.py
+++ charge-backend/retro_tool_server.py
@@ -5,7 +5,7 @@ import os
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(cur_dir, "ChARGe", "experiments", "Retrosynthesis"))
 
-import ChARGe.experiments.Retrosynthesis.retro_server as RETRO_MCP
+import ChARGe.experiments.Retrosynthesis.reaction_server as RETRO_MCP
 import charge.servers.server_utils as server_utils
 from charge.servers.AizynthTools import is_molecule_synthesizable
 
