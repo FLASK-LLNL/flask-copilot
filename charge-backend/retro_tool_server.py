80335547ed14ec3aecf4c7bca0ac4c80a9de7787
Shehtab <szaman5@binghamton.edu>
2025-10-12 23:30:48 -0400

    Updated readme and retro tool server

--- charge-backend/retro_tool_server.py
+++ charge-backend/retro_tool_server.py
@@ -14,7 +14,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Run a ChARGe MCP Server")
 parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
 parser.add_argument(
-    "--host", type=str, default="http://127.0.0.1", help="Host to run the server on"
+    "--host", type=str, default="127.0.0.1", help="Host to run the server on"
 )
 parser.add_argument(
     "--config",
