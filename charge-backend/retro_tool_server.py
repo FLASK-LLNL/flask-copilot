a85c7488b6df1a6c60a429ace87de6b899f35c1b
Shehtab <szaman5@binghamton.edu>
2025-10-12 21:00:25 -0400

    Integreted aizynthfinder and charge workflow with UI

--- charge-backend/retro_tool_server.py
+++ charge-backend/retro_tool_server.py
@@ -2,12 +2,14 @@ import sys
 import os
 
 
+import charge.servers.server_utils as server_utils
+from charge.servers.AiZynthTools import is_molecule_synthesizable
+
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(cur_dir, "ChARGe", "experiments", "Retrosynthesis"))
 
 import ChARGe.experiments.Retrosynthesis.reaction_server as RETRO_MCP
-import charge.servers.server_utils as server_utils
-from charge.servers.AizynthTools import is_molecule_synthesizable
+
 
 port = server_utils.args.port
 host = server_utils.args.host
@@ -16,6 +18,7 @@ mcp = RETRO_MCP.template_free_mcp
 
 mcp.settings.port = port
 mcp.settings.host = host
+mcp._setup_handlers()
 
 mcp.tool()(is_molecule_synthesizable)
 
