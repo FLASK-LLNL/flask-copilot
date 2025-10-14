35ae3a8c0f3880a562e0864f1b035fb0367b9d6a
Brian C. Van Essen <vanessen1@llnl.gov>
2025-10-13 17:11:27 -0700

    Added the ability to specify the host and port used for each server.

--- charge-backend/lmo_tool_server.py
+++ charge-backend/lmo_tool_server.py
@@ -6,7 +6,23 @@ sys.path.append(os.path.join(cur_dir, "ChARGe", "experiments", "Molecule_Generat
 
 import ChARGe.experiments.Molecule_Generation.mol_server as LMO_MCP
 
+import argparse
+
+parser = argparse.ArgumentParser(description="Run a ChARGe MCP Server")
+parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
+parser.add_argument(
+    "--host", type=str, default="127.0.0.1", help="Host to run the server on"
+)
+args = parser.parse_args()
+
+
+port = args.port
+host = args.host
+
 mcp = LMO_MCP.mcp
+
+mcp.settings.port = port
+mcp.settings.host = host
 
 if __name__ == "__main__":
     mcp.run(transport="sse")
