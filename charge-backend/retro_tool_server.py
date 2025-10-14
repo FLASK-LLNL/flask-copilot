d8fc73d89ac3c9d3d6cdf93ca07356e8a6506e4c
Brian C. Van Essen <vanessen1@llnl.gov>
2025-10-13 22:05:06 -0700

    Updated the FLASK copilot servers: charge, retro, and lmo to auto-detect their hostnames.

--- charge-backend/retro_tool_server.py
+++ charge-backend/retro_tool_server.py
@@ -8,34 +8,36 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(cur_dir, "ChARGe", "experiments", "Retrosynthesis"))
 
 import ChARGe.experiments.Retrosynthesis.reaction_server as RETRO_MCP
-
-import argparse
-
-parser = argparse.ArgumentParser(description="Run a ChARGe MCP Server")
-parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
-parser.add_argument(
-    "--host", type=str, default="127.0.0.1", help="Host to run the server on"
-)
-parser.add_argument(
-    "--config",
-    type=str,
-    default="config.yml",
-    help="Path to the configuration file for AiZynthFinder",
-)
-args = parser.parse_args()
-
-
-port = args.port
-host = args.host
-
-mcp = RETRO_MCP.template_free_mcp
-
-RetroPlanner.initialize(configfile=args.config)
-mcp.tool()(is_molecule_synthesizable)
-
-
-mcp.settings.port = port
-mcp.settings.host = host
+from ChARGe.charge.servers.server_utils import update_mcp_network, get_hostname
 
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Run a ChARGe MCP Server")
+    parser.add_argument("--port", type=int, default=8123, help="Port to run the server on")
+    parser.add_argument(
+        "--host", type=str, default=None, help="Host to run the server on"
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="config.yml",
+        help="Path to the configuration file for AiZynthFinder",
+    )
+    args = parser.parse_args()
+
+    port = args.port
+    host = args.host
+    if host is None:
+        _, host = get_hostname()
+
+    mcp = RETRO_MCP.template_free_mcp
+
+    RetroPlanner.initialize(configfile=args.config)
+    mcp.tool()(is_molecule_synthesizable)
+
+    if host is None:
+        _, host = get_hostname()
+
+    update_mcp_network(mcp, host, port)
+
     mcp.run(transport="sse")
