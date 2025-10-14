78bc7202de5a84a7978352beeb6c88ab8418850f
Brian Van Essen <vanessen1@llnl.gov>
2025-10-13 22:24:20 -0700

    Servers auto-detect network (#12)
    * Updated the FLASK copilot servers: charge, retro, and lmo to
    auto-detect their hostnames.
    
    * Updated submodule.

--- charge-backend/lmo_tool_server.py
+++ charge-backend/lmo_tool_server.py
@@ -5,24 +5,27 @@ cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(cur_dir, "ChARGe", "experiments", "Molecule_Generation"))
 
 import ChARGe.experiments.Molecule_Generation.mol_server as LMO_MCP
+from ChARGe.charge.servers.server_utils import update_mcp_network, get_hostname
 
-import argparse
-
-parser = argparse.ArgumentParser(description="Run a ChARGe MCP Server")
-parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
-parser.add_argument(
-    "--host", type=str, default="127.0.0.1", help="Host to run the server on"
-)
-args = parser.parse_args()
-
-
-port = args.port
-host = args.host
-
-mcp = LMO_MCP.mcp
-
-mcp.settings.port = port
-mcp.settings.host = host
 
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Run a ChARGe MCP Server")
+    parser.add_argument("--port", type=int, default=8124, help="Port to run the server on")
+    parser.add_argument(
+        "--host", type=str, default=None, help="Host to run the server on"
+    )
+    args = parser.parse_args()
+
+    port = args.port
+    host = args.host
+
+    mcp = LMO_MCP.mcp
+
+    if host is None:
+        _, host = get_hostname()
+
+    update_mcp_network(mcp, host, port)
+
     mcp.run(transport="sse")
