c44d3e3504d214ed823ed01423cc29073a071eb4
Tal Ben-Nun <tbennun@users.noreply.github.com>
2025-11-21 07:47:31 -0800

    Several deployment fixes (#61)

--- dockerscripts/launch_servers.sh
+++ dockerscripts/launch_servers.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+. /venv/bin/activate
+
 # Launch main server
-uvicorn $@ &
+export PYTHONPATH=$PYTHONPATH:/app/charge-backend
+uvicorn --host 0.0.0.0 --port 8001 --workers 8 charge_server:app &
 
 # Wait for server to start up
 sleep 10
 
 # Launch MCP servers
-python3 /app/charge-backend/retro_tool_server.py --config /aizynth/config.yml --copilot-host localhost --copilot-port 8001 &
-python3 /app/charge-backend/lmo_tool_server.py --copilot-host localhost --copilot-port 8001
+python /app/charge-backend/retro_tool_server.py --config /aizynth/config.yml --copilot-host localhost --copilot-port 8001 &
+python /app/charge-backend/lmo_tool_server.py --copilot-host localhost --copilot-port 8001
