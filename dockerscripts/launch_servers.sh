d04d5e5462cacd32697f7f7bbf18232f039f7f63
Tal Ben-Nun <tbennun@users.noreply.github.com>
2026-01-29 21:47:58 -0800

    Add root folder to PYTHONPATH (#144)

--- dockerscripts/launch_servers.sh
+++ dockerscripts/launch_servers.sh
@@ -3,7 +3,7 @@
 . /venv/bin/activate
 
 # Launch main server
-export PYTHONPATH=$PYTHONPATH:/app/charge_backend
+export PYTHONPATH=$PYTHONPATH:/app/charge_backend:/app
 uvicorn --host 0.0.0.0 --port 8001 --workers 8 charge_server:app &
 
 # Wait for server to start up
