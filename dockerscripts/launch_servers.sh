1e65178ce9ea4aba05847e69b1ac0a56759e77b2
Brian Van Essen <vanessen1@llnl.gov>
2026-02-21 15:03:56 -0800

    Removed the launch of the MCP servers from the container. (#166)

--- dockerscripts/launch_servers.sh
+++ dockerscripts/launch_servers.sh
@@ -4,11 +4,4 @@
 
 # Launch main server
 export PYTHONPATH=$PYTHONPATH:/app/charge_backend:/app
-uvicorn --host 0.0.0.0 --port 8001 --workers 8 charge_server:app &
-
-# Wait for server to start up
-sleep 10
-
-# Launch MCP servers
-python /app/charge_backend/retro_tool_server.py --config /data/config.yml --copilot-host localhost --copilot-port 8001 &
-python /app/charge_backend/lmo_tool_server.py --copilot-host localhost --copilot-port 8001
+uvicorn --host 0.0.0.0 --port 8001 --workers 8 charge_server:app
